255c0b83c93515b33281615aa41469efaeecfb89
glsl7503 <gim325901@gmail.com>
2025-09-20 13:15:32 +0900

    fix: 엔트리 포인트 통합

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// 보고 싶은 앱 선택
-// import App from "./tic-tac-toe/App";
 import App from "./list-grid-view/App";
+// import Game from "./tic-tac-toe/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App /> 
-  </React.StrictMode>
-);
+root.render(<App/>);
